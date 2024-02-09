8551df25c424f61f358bdb6aa50fae962cb16fe9
pavlo <pashapasha600@gmail.com>
2024-02-09 12:55:03 +0100

    task2,3_fix

--- js/task-2.js
+++ js/task-2.js
@@ -14,7 +14,7 @@ function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
         return message;
     } else {
-        const truncatedMessage = message.slice(0, maxLength - 3) + '...';
+        const truncatedMessage = message.slice(0, maxLength ) + '...';
         return truncatedMessage;
     }
 }
--- js/task-3.js
+++ js/task-3.js
@@ -17,9 +17,9 @@
 function checkForSpam(message) {
     const lowerCaseMessage  = message.toLowerCase();
     if  (lowerCaseMessage.includes('spam') || lowerCaseMessage.includes('sale')) {
-        return 'true';
+        return true;
     } else {
-        return 'false';
+        return false;
     }
 }
 
